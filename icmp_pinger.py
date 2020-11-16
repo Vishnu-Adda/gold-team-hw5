954c9f29714753ed7665029dd84f122c0ea4e7e5
Vishnu Adda <addavishnu@gmail.com>
2020-11-15 20:17:38 -0800

    Commit the whole code for this lab.

---
+++ icmp_pinger.py
@@ -0,0 +1,122 @@
+from socket import *
+import os
+import sys
+import struct
+import time
+import select
+import binascii
+
+
+ICMP_ECHO_REQUEST = 8
+
+
+def checksum(string):
+    csum = 0
+    countTo = (len(string) // 2) * 2
+    count = 0
+
+    while count < countTo:
+        thisVal = string[count + 1] * 256 + string[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+        count = count + 2
+    
+    if countTo < len(string):
+        csum = csum + string[len(string) - 1]
+        csum = csum & 0xffffffff
+    
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
+
+
+def receiveOnePing(mySocket, ID, timeout, destAddr):
+    timeLeft = timeout
+
+    while 1:
+        startedSelect = time.time()
+        whatReady = select.select([mySocket], [], [], timeLeft)
+        howLongInSelect = (time.time() - startedSelect)
+        if whatReady[0] == []:  # Timeout
+            return "Request timed out."
+
+        timeReceived = time.time()
+        recPacket, addr = mySocket.recvfrom(1024)
+
+        #Fill in start
+
+        #Fetch the ICMP header from the IP packet
+
+        # ICMP header starts after bit 160, i.e byte 20 to byte 28
+        header = recPacket[20:28]
+        type, code, checksum, headerID, sequence = struct.unpack("!bbHHh", header)
+
+        # Echo reply: type & code == 0
+        if type == 0 and code == 0 and ID == headerID:
+            # Need the size to get the timeSent data
+            size = struct.calcsize("!d")
+            # Gives the time the packet was sent
+            timeSent = struct.unpack("!d", recPacket[28:28 + size])[0]
+            # Get the TTL from the IP header
+            ttl = ord(struct.unpack("!c", recPacket[8:9])[0])
+            return ("RTT = " + str(timeReceived - timeSent), "TTL = " + str(ttl))
+        else:
+            return "Type is not 0 and / or Code is not 0 and / or ID does not match"
+        #Fill in end
+        timeLeft = timeLeft - howLongInSelect
+        if timeLeft <= 0:
+            return "Request timed out."
+
+
+def sendOnePing(mySocket, destAddr, ID):
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+
+    myChecksum = 0
+    # Make a dummy header with a 0 checksum.
+    # struct -- Interpret strings as packed binary data
+    header = struct.pack("!bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    data = struct.pack("!d", time.time())
+    # Calculate the checksum on the data and the dummy header.
+    myChecksum = checksum(header + data)
+
+    header = struct.pack("!bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+    packet = header + data
+
+    mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
+    # Both LISTS and TUPLES consist of a number of objects
+    # which can be referenced by their position number within the object
+
+
+def doOnePing(destAddr, sequence, timeout):
+    icmp = getprotobyname("icmp")
+
+    # SOCK_RAW is a powerful socket type. For more details see: http://sock-raw.org/papers/sock_raw
+
+    mySocket = socket(AF_INET, SOCK_RAW, icmp)
+
+    myID = os.getpid() & 0xFFFF # Return the current process i
+    sendOnePing(mySocket, destAddr, myID)
+    delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+
+    mySocket.close()
+    return delay
+
+
+def ping(host, timeout=1):
+    # timeout=1 means: If one second goes by without a reply from the server,
+    # the client assumes that either the client’s ping or the server’s pong is lost
+    dest = gethostbyname(host)
+    print("Pinging " + dest + " using Python:")
+    print("")
+    # Send ping requests to a server separated by approximately one second
+    while 1 :
+        delay = doOnePing(dest, 1, timeout)
+        print(delay)
+        time.sleep(1)# one second
+    return delay
+
+
+ping("nsw.gov.au")
